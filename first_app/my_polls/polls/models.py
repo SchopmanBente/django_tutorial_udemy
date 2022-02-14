92e4c65dfe16da163a313f42edd5cd964ba47b12
Bente Schopman <b.l.schopman@gmail.com>
2022-02-14 15:20:31 +0100

    Section 4, lesson 9

--- first_app/my_polls/polls/models.py
+++ first_app/my_polls/polls/models.py
@@ -9,6 +9,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the Questions
+        context['question_list'] = Question.objects.all()
+        return context
+
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
--- first_app/my_polls/polls/views.py
+++ first_app/my_polls/polls/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpResponse
+#from django.template import loader,RequestContext
 from .models import Question
+from django.views.generic import ListView
 
 # Create your views here.
+"""
 def index(request):
     latest_questions = Question.objects.order_by('-publish_date')[:5]
-    output = ",".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+     'latest_questions': latest_questions
+    })
+    servertemplate = loader.select_template(template).template
 
-
+    return HttpResponse(servertemplate)
+*/
+"""
+class QuestionsListView(ListView):
+    model = Question
+    context_object_name = "Available polls"
 def detail(request, question_id):
     return HttpResponse("This is the detail view of question: %s" % question_id)
 
