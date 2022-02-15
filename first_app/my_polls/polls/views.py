07a0d1bf8bae2cbb819712d61ae5b8ff2d1022c6
Bente Schopman <b.l.schopman@gmail.com>
2022-02-15 16:17:07 +0100

    Code from section 4, lesson 10, cleaner code

--- first_app/my_polls/polls/views.py
+++ first_app/my_polls/polls/views.py
@@ -1,30 +1,25 @@
 from django.http import HttpResponse
 #from django.template import loader,RequestContext
-from django.shortcuts import render
+from django.http import Http404
+
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 from django.views.generic import ListView
 
 # Create your views here.
-"""
-def index(request):
-    latest_questions = Question.objects.order_by('-publish_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-     'latest_questions': latest_questions
-    })
-    servertemplate = loader.select_template(template).template
 
-    return HttpResponse(servertemplate)
-*/
-"""
 class QuestionsListView(ListView):
     model = Question
     context_object_name = "Available polls"
 
 def detail(request, question_id):
-    q = Question.objects.get(pk=question_id)
-    return render(request,"polls/detail.html",{"question":q})
+    try:
+        q = Question.objects.get(pk=question_id)
+        return render(request, "polls/detail.html", {"question": q})
+    except Question.DoesNotExist:
+        raise Http404("No Question matches the given query.")
+
 
 
 def results(request, question_id):
