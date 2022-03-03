5ea53651ec6be6de202ba8edf00b074812793354
Bente Schopman <b.l.schopman@gmail.com>
2022-03-03 17:35:49 +0100

    Working code from section 4, lesson 11

--- first_app/my_polls/polls/urls.py
+++ first_app/my_polls/polls/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
-from .views import QuestionsListView, VoteView, ResultsView
+from .views import QuestionsListView
 
 app_name = 'polls'
 
 urlpatterns = [
     path('', QuestionsListView.as_view(), name="index"),
     path('<int:question_id>', views.detail, name="detail"),
-    path('<int:question_id>/vote', VoteView.as_view(), name="vote"),
-    #path('<int:question_id>/results', ResultsView.as_view(), name="results")
+    path('<int:question_id>/vote', views.vote, name="vote"),
     path('<int:question_id>/results', views.results, name="results")
 ]
--- first_app/my_polls/polls/views.py
+++ first_app/my_polls/polls/views.py
@@ -2,16 +2,15 @@
 
 # from django.template import loader,RequestContext
 
-from django.http import Http404 , HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404 , reverse
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_protect
-from django.views.generic import ListView, UpdateView, View
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views.generic import ListView, View
 
 from .models import Question, Choice
 
 
 # Create your views here.
+
 
 class QuestionsListView(ListView):
     model = Question
@@ -26,44 +25,28 @@ def detail(request, question_id):
         raise Http404("No Question matches the given query.")
 
 
-def results(request, polls, question_id):
+
+def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    choosen_choice = get_object_or_404(Choice, pk=polls)
-    return render(request, 'polls/results-1.html', {'question': question, 'choosen_choice': choosen_choice, })
+    return render(request, 'polls/results-1.html', {'question': question,})
 
-
+""" 
 class ResultsView(View):
-    model = Choice
+    model = Question
     template_name = 'polls/results-1.html'
-
+"""
 
 def vote(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk= request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["polls"])
     except:
-        return  render(request, "polls/detail-1.html", {'question': question, 'error_message':"Please select a choice!"})
+        return render(request, "polls/detail-1.html",
+                      {'question': question, 'error_message': "Please select a choice!"})
+
     else:
-        selected_choice.votes +=1
+        selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls/results-1.html', args={question.id}) )
 
-
-@method_decorator(csrf_protect, name='dispatch')
-class VoteView(UpdateView):
-    model = Choice
-    fields = [
-        "choice_text",
-        "votes",
-        "question",
-    ]
-    success_url = "<int:question_id>/results"
-    template_name = "results-1.html"
-
-    def post(self, request, **kwargs):
-        # question_id = request.POST['question']
-        # question = get_object_or_404(Question, pk=question_id)
-
-        model = request.POST.get('question_id')
-        choice_id = request.POST.get('choice')
-        return HttpResponseRedirect(request, 'polls/results-1.html', {'question_id': model, 'choice_id': choice_id, })
+        return HttpResponseRedirect(reverse('polls:results', args={question.id}))
+        # return '{}<int:question_id>/results'.format(reverse('polls:results', kwargs={'question': question.pk,}))
